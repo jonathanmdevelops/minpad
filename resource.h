11206a82754c6ed3e1c8119d729ae7ea45fe75c2
saint-thomas-more <saint-thomas-more@github.com>
2020-06-28 13:05:33 +0100

    Initial commit.

---
+++ resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MinPad.rc
+//
+#define IDC_MYICON                      2
+#define IDD_MINPAD_DIALOG               102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MINPAD                      107
+#define IDI_SMALL                       108
+#define IDC_MINPAD                      109
+#define IDR_MAINFRAME                   128
+#define ID_FILE_OPENFILE                32771
+#define IDM_OPEN_FILE                   32772
+#define IDC_STATIC                      -1
+
+#define IDM_OPEN_FILE                       110
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
