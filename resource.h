fd6bd5e09bd87ac1ad010c372165fd0b3ad65468
saint-thomas-more <saint-thomas-more@github.com>
2020-06-30 15:51:27 +0100

    Improved resource handling.

--- resource.h
+++ resource.h
@@ -1,34 +1,31 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by MinPad.rc
+// Used by Resource.rc
 //
-#define IDC_MYICON                      2
-#define IDD_MINPAD_DIALOG               102
-#define IDS_APP_TITLE                   103
-#define IDD_ABOUTBOX                    103
-#define IDM_ABOUT                       104
-#define IDM_EXIT                        105
-#define IDI_MINPAD                      107
-#define IDI_SMALL                       108
-#define IDC_MINPAD                      109
-#define IDM_OPEN_FILE                   110
-#define IDM_NEW_FILE                    111
-#define IDM_SAVE_FILE                   112
-#define IDR_MAINFRAME                   128
-#define ID_FILE_OPENFILE                32771
-#define ID_FILE_SAVEFILE                32773
-#define ID_FILE_NEWFILE                 32774
-#define IDC_EDIT                        32775
-#define IDC_STATIC                      -1
+#define IDR_MENU                        104
+#define IDD_ABOUT                       105
+#define IDS_APP_TITLE                   107
+#define IDI_LARGE                       108
+#define IDI_MINPAD                      108
+#define IDR_ACCELERATOR                 113
+#define IDD_ABOUT_OK                    1002
+#define IDD_ABOUT_TEXT                  1003
+#define IDM_FILE_NEW                    40002
+#define IDM_FILE_OPEN                   40003
+#define IDM_FILE_SAVE                   40004
+#define IDM_FILE_EXIT                   40005
+#define IDM_HELP_ABOUT                  40006
+#define ID_ACCELERATOR_OPEN             40017
+#define ID_ACCELERATOR_SAVE             40018
+#define ID_ACCELERATOR_NEW              40019
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32776
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           110
+#define _APS_NEXT_RESOURCE_VALUE        114
+#define _APS_NEXT_COMMAND_VALUE         40021
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
